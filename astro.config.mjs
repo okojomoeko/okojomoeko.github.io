0f7986961e2f820c96cdcd0eec31b49b71ba712b
okojomoeko <chikoro.size@gmail.com>
2023-04-02 22:54:09 +0900

    Googleanalygics (#56) (#57)
    * add GA
    
    * add prepare for playwright
    
    * add threshold

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://okojomoeko.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
